5c73b8f8da549171d5ffa54ba8b17c0453fe2870
mestredainfo <>
2024-05-17 17:26:24 -0300

    Adicionado informações de licença

--- criaratalho.php
+++ criaratalho.php
@@ -1,4 +1,10 @@
 <?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Organização: Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+
 $sFolder = '/home/' . get_current_user() . '/.local/share/applications/';
 if (file_exists($sFolder)) {
     $sDesktop = "[Desktop Entry]\n
