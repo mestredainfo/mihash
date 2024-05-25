9d836fa4fc835cc8c463428f0a8a0610dfdc7d47
mestredainfo <>
2024-05-25 19:08:22 -0300

    Ajustado o código para funcionar no Windows também.

--- criaratalho.php
+++ criaratalho.php
@@ -5,9 +5,10 @@
 // Organização: Mestre da Info
 // Site: https://linktr.ee/mestreinfo
 
-$sFolder = '/home/' . get_current_user() . '/.local/share/applications/';
-if (file_exists($sFolder)) {
-    $sDesktop = "[Desktop Entry]\n
+if (PHP_OS == 'LINUX') {
+    $sFolder = '/home/' . get_current_user() . '/.local/share/applications/';
+    if (file_exists($sFolder)) {
+        $sDesktop = "[Desktop Entry]\n
 Version=" . file_get_contents(dirname(__FILE__) . '/version') . "\n
 Name=MIHash\n
 Comment=Verifica e gera hash de arquivos\n
@@ -16,12 +17,15 @@ Exec=" . dirname(__FILE__, 4) . "/mihash\n
 Icon=" . dirname(__FILE__, 2) . "/icon/mihash.png\n
 Categories=Utility;";
 
-    $sCreateFile = file_put_contents($sFolder . '/mihash.desktop', $sDesktop);
-    if ($sCreateFile) {
-        echo '<script>window.alert(\'Atalho criado no menu iniciar!\');window.location.assign(\'index.php\');</script>';
+        $sCreateFile = file_put_contents($sFolder . '/mihash.desktop', $sDesktop);
+        if ($sCreateFile) {
+            echo '<script>window.alert(\'Atalho criado no menu iniciar!\');window.location.assign(\'index.php\');</script>';
+        } else {
+            echo '<script>window.alert(\'Não foi possível criar o atalho no menu iniciar!\');window.location.assign(\'index.php\');</script>';
+        }
     } else {
         echo '<script>window.alert(\'Não foi possível criar o atalho no menu iniciar!\');window.location.assign(\'index.php\');</script>';
     }
 } else {
-    echo '<script>window.alert(\'Não foi possível criar o atalho no menu iniciar!\');window.location.assign(\'index.php\');</script>';
+    echo '<script>window.alert(\'No Windows você pode criar um atalho clicando com o botão direito no executável "mihash.exe" e clicando em "Criar Atalho"!\');window.location.assign(\'index.php\');</script>';
 }
