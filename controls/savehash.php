491d7f84673bddb957797e5f53004b497a43600e
mestredainfo <>
2024-06-08 12:09:49 -0300

    Criado o arquivo savehash.php

---
+++ controls/savehash.php
@@ -0,0 +1,20 @@
+<?php
+// Copyright (C) 2004-2024 Murilo Gomes Julio
+// SPDX-License-Identifier: GPL-2.0-only
+
+// Organização: Mestre da Info
+// Site: https://linktr.ee/mestreinfo
+
+header("Content-Security-Policy: default-src 'self'");
+header("Content-Security-Policy: script-src 'self' 'unsafe-inline' script.js");
+
+if (miRequestPost()) {
+    $sArquivo = miCleanPost('arquivo');
+    $sFile = pathinfo(miCleanPost('file'));
+    $sTipo = miCleanPost('tipo');
+    $sHash = miCleanPost('hash');
+
+    file_put_contents(sprintf('%s.%s', $sArquivo, $sTipo), sprintf('%s %s', $sHash, $sFile['basename']));
+
+    echo '<div class="alert alert-info">' . miTranslate('Arquivo gerado com sucesso!') . '</div>';
+}
